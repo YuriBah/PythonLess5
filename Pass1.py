284c79e6aa0c5653c49f177ed29dbe5fba17a3ef
YuriBah <77458955+YuriBah@users.noreply.github.com>
2021-11-26 21:14:04 +0300

    Add files via upload

---
+++ Pass1.py
@@ -0,0 +1,13 @@
+def odd_nums(num):
+    for num in range(1, num + 1, 2):
+        yield num
+
+
+for i in odd_nums(15):
+    print(i)
+
+#-------------------------------------------2------------------------------------------
+
+n = int(input())
+
+print(*[num for num in range(1, int(input()) + 1, 2)])
---
+++ Pass2.py
@@ -0,0 +1,15 @@
+from itertools import islice, zip_longest
+
+tutors = [
+    'Иван', 'Анастасия', 'Петр', 'Сергей',
+    'Дмитрий', 'Борис', 'Елена'
+]
+klasses = [
+    '9А', '7В', '9Б', '9В', '8Б', '10А', '10Б', '9А'
+]
+
+rezult = (i for i in zip_longest(tutors, klasses))
+
+print(type(rezult))
+print(*islice(rezult, 9))
+print(list(islice(rezult, 3)))
